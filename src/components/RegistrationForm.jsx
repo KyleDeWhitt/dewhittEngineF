7c28722293723419fa80efcc771ac17c1b9de814
Kyle DeWhitt <kyle@example.com>
2025-11-26 17:56:59 -0800

    Fix casing for AuthContext import

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom'; 
-import { useAuth } from '../context/authcontext'; 
+import { useAuth } from '../context/AuthContext'; 
 
 function RegistrationForm() {
   const { registerUser } = useAuth(); 
